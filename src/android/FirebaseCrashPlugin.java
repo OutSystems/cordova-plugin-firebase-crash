0d8480ba541f43aeabb21383b22867ae5ae52c76
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2025-05-02 15:54:10 +0100

    refactor: Remove dependency on cordova-support-android-plugin
    By making the native android plugin extend just from plain CordovaPlugin
    
    References: https://outsystemsrd.atlassian.net/browse/RMET-3644

--- src/android/FirebaseCrashPlugin.java
+++ src/android/FirebaseCrashPlugin.java
@@ -2,17 +2,15 @@ package by.chemerisuk.cordova.firebase;
 
 import android.util.Log;
 
-import by.chemerisuk.cordova.support.CordovaMethod;
-import by.chemerisuk.cordova.support.ReflectiveCordovaPlugin;
-
 import com.google.firebase.crashlytics.FirebaseCrashlytics;
 
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 
 
-public class FirebaseCrashPlugin extends ReflectiveCordovaPlugin {
+public class FirebaseCrashPlugin extends CordovaPlugin {
     private final String TAG = "FirebaseCrashPlugin";
 
     private FirebaseCrashlytics firebaseCrashlytics;
@@ -25,45 +23,73 @@ public class FirebaseCrashPlugin extends ReflectiveCordovaPlugin {
             firebaseCrashlytics = FirebaseCrashlytics.getInstance();
         }
         catch (Exception e){
-            Log.e("Firebase Crashlytics Plugin", e.getMessage());
+            Log.e(TAG, "Unable to instantiate Crashlytics", e);
         }
     }
 
-    @CordovaMethod(ExecutionThread.WORKER)
-    private void forceCrash(CallbackContext callbackContext) {
-            new Thread(){
-                @Override
-                public void run() {
-                    throw new RuntimeException("MyCrash");
-                }
-            }.start();
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        switch (action) {
+            case "forceCrash":
+                forceCrash();
+                break;
+            case "log":
+                String message = args.optString(0);
+                log(message, callbackContext);
+                break;
+            case "logError":
+                String errorMessage = args.optString(0);
+                logError(errorMessage, callbackContext);
+                break;
+            case "setUserId":
+                String userId = args.optString(0);
+                setUserId(userId, callbackContext);
+                break;
+            case "setEnabled":
+                boolean isEnabled = args.getBoolean(0);
+                setEnabled(isEnabled, callbackContext);
+                break;
+            default:
+                return super.execute(action, args, callbackContext);
+        }
+        return true;
     }
 
-    @CordovaMethod(ExecutionThread.WORKER)
+    private void forceCrash() {
+        cordova.getThreadPool().execute(() -> {
+            throw new RuntimeException("MyCrash");
+        });
+    }
+
     private void log(String message, CallbackContext callbackContext) {
-        if(firebaseCrashlytics != null){
-            firebaseCrashlytics.log(message);
-            callbackContext.success();
-        }
+        cordova.getThreadPool().execute(() -> {
+            if(firebaseCrashlytics != null){
+                firebaseCrashlytics.log(message);
+                callbackContext.success();
+            }
+        });
     }
 
-    @CordovaMethod(ExecutionThread.UI)
     private void logError(String message, CallbackContext callbackContext) {
-        if(firebaseCrashlytics != null){
-            firebaseCrashlytics.recordException(new Exception(message));
-            callbackContext.success();
-        }
+        cordova.getActivity().runOnUiThread(() -> {
+            if(firebaseCrashlytics != null){
+                Exception error = new Exception(message);
+                Log.d(TAG, "Logging non-fatal error to crashlytics", error);
+                firebaseCrashlytics.recordException(error);
+                callbackContext.success();
+            }
+        });
     }
 
-    @CordovaMethod(ExecutionThread.UI)
     private void setUserId(String userId, CallbackContext callbackContext) {
-        if(firebaseCrashlytics != null){
-            firebaseCrashlytics.setUserId(userId);
-            callbackContext.success();
-        }
+        cordova.getActivity().runOnUiThread(() -> {
+            if(firebaseCrashlytics != null){
+                firebaseCrashlytics.setUserId(userId);
+                callbackContext.success();
+            }
+        });
     }
 
-    @CordovaMethod
     private void setEnabled(boolean enabled, CallbackContext callbackContext) {
         if(firebaseCrashlytics != null){
             firebaseCrashlytics.setCrashlyticsCollectionEnabled(enabled);
