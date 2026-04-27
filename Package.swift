eb5f35fefe4a83ef56096f3164e79ab728faa15d
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2026-04-27 13:10:08 +0100

    feat: Add SPM support

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "cordova-plugin-firebase-crash",
+    platforms: [.iOS(.v15)],
+    products: [
+        .library(
+            name: "cordova-plugin-firebase-crash",
+            targets: ["cordova-plugin-firebase-crash"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apache/cordova-ios.git", branch: "master"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", exact: "10.29.0")
+    ],
+    targets: [
+        .target(
+            name: "cordova-plugin-firebase-crash",
+            dependencies: [
+                .product(name: "Cordova", package: "cordova-ios"),
+                .product(name: "FirebaseCrashlytics", package: "firebase-ios-sdk")
+            ],
+            path: "src/ios",
+            publicHeadersPath: ".")
+    ]
+)
