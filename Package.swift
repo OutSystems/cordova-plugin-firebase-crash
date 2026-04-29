d7b8162aede00645619ac5b0f947455a89efc138
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2026-04-29 10:15:46 +0100

    chore: correct Package.swift firebase dependency

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apache/cordova-ios.git", branch: "master"),
-        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", exact: "10.29.0")
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "10.29.0")
     ],
     targets: [
         .target(
